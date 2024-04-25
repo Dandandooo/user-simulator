62e1ecf977efc2f9011e5a0befcc51e3a9a4e599
Daniel Philipov <batkodanny@gmail.com>
2024-04-24 23:59:34 -0500

    da functionality in user_sim prompt

--- src/data/parse/parse_turn.py
+++ src/data/parse/parse_turn.py
@@ -23,18 +23,34 @@ def convert_dialogue(dialogue_action: dict) -> dict:
     }
     return action
 
-def convert_generic(dialogue_action: dict, action: str) -> dict:
-    agent = {0: "COMMANDER", 1: "DRIVER"}[dialogue_action['agent_id']]
-    action = {
+def convert_generic(generic_action: dict, action: str) -> dict:
+    agent = {0: "COMMANDER", 1: "DRIVER"}[generic_action['agent_id']]
+    act = {
         agent: {
-            'action': action,
+            'action': f'<{action}>',
         },
         "COMMANDER" if agent == "DRIVER" else "DRIVER": {
             'action': '<observe>',
         },
         'turn_action': action
     }
-    return action
+    return act
+
+# Pass in action without the < > tags
+def convert_object(object_action: dict, action: str) -> dict:
+    agent = {0: "COMMANDER", 1: "DRIVER"}[object_action['agent_id']]
+    obj = object_action['oid'].split('|')[0]  # Assuming this is the case for every example
+    act = {
+        agent: {
+            'action': f'<{action} {obj}>',
+            'object': obj
+        },
+        "COMMANDER" if agent == "DRIVER" else "DRIVER": {
+            'action': '<observe>',
+        },
+        'turn_action': action
+    }
+    return act
 
 
 def parse_game(filename: str) -> dict:
@@ -44,16 +60,17 @@ def parse_game(filename: str) -> dict:
     data['goal'] = task['desc']
     # Action ID 100 is a dialogue action
     turns = data['turns'] = []
+    # print(filename)
     for interaction in task['episodes'][0]['interactions']:
         match interaction['action_id']:
             case 100:
                 turns.append(convert_dialogue(interaction))
             case 0:
-                turns.append(convert_generic(interaction, '<stop moving>'))
+                turns.append(convert_generic(interaction, 'stop moving'))
             case 1 | 2 | 3 | 10 | 11 | 12 | 13:
-                if turns and turns[-1]['turn_action'] == '<move>':
+                if turns and turns[-1]['turn_action'] == 'move':
                     continue
-                turns.append(convert_generic(interaction, '<move>'))
+                turns.append(convert_generic(interaction, 'move'))
             # case 4 | 5:
             #     if turns and turns[-1]['turn_action'] == '<turn>':
             #         continue
@@ -63,31 +80,57 @@ def parse_game(filename: str) -> dict:
             #         continue
             #     turns.append(convert_generic(interaction, '<look around>'))
             case 200:
-                turns.append(convert_generic(interaction, '<pickup>'))
+                if interaction["oid"] is None:
+                    continue
+                turns.append(convert_object(interaction, 'pickup'))
             case 201:
-                turns.append(convert_generic(interaction, '<putdown>'))
+                if interaction["oid"] is None:
+                    continue
+                turns.append(convert_object(interaction, 'putdown'))
             case 202:
-                turns.append(convert_generic(interaction, '<open>'))
+                if interaction["oid"] is None:
+                    continue
+                turns.append(convert_object(interaction, 'open'))
             case 203:
-                turns.append(convert_generic(interaction, '<close>'))
+                if interaction["oid"] is None:
+                    continue
+                turns.append(convert_object(interaction, 'close'))
             case 204:
-                turns.append(convert_generic(interaction, '<toggle on>'))
+                if interaction["oid"] is None:
+                    continue
+                turns.append(convert_object(interaction, 'toggle on'))
             case 205:
-                turns.append(convert_generic(interaction, '<toggle off>'))
+                if interaction["oid"] is None:
+                    continue
+                turns.append(convert_object(interaction, 'toggle off'))
             case 206:
-                turns.append(convert_generic(interaction, '<slice>'))
+                if interaction["oid"] is None:
+                    continue
+                turns.append(convert_object(interaction, 'slice'))
             case 207:
-                turns.append(convert_generic(interaction, '<dirty>'))
+                if interaction["oid"] is None:
+                    continue
+                turns.append(convert_object(interaction, 'dirty'))
             case 208:
-                turns.append(convert_generic(interaction, '<clean>'))
+                if interaction["oid"] is None:
+                    continue
+                turns.append(convert_object(interaction, 'clean'))
             case 209:
-                turns.append(convert_generic(interaction, '<fill>'))
+                if interaction["oid"] is None:
+                    continue
+                turns.append(convert_object(interaction, 'fill'))
             case 210:
-                turns.append(convert_generic(interaction, '<empty>'))
+                if interaction["oid"] is None:
+                    continue
+                turns.append(convert_object(interaction, 'empty'))
             case 211:
-                turns.append(convert_generic(interaction, '<pour>'))
+                if interaction["oid"] is None:
+                    continue
+                turns.append(convert_object(interaction, 'pour'))
             case 212:
-                turns.append(convert_generic(interaction, '<break>'))
+                if interaction["oid"] is None:
+                    continue
+                turns.append(convert_object(interaction, 'break'))
             case _:
                 # I don't know how to handle anything else yet
                 pass
--- src/prompt_llm/user_sim.py
+++ src/prompt_llm/user_sim.py
@@ -14,6 +14,55 @@ class TurnMaker:
         self.numerate = kwargs["numerate"]
         self.save_path = kwargs["save_path"]
         self.give_task = kwargs["give_task"]
+        self.include_das = kwargs["include_das"]
+        self.nl_ify = kwargs["nl_ify"]
+        self.html_format = kwargs["html_format"]
+        self.predict_das = kwargs["predict_das"]
+
+        if self.nl_ify:
+            self.das = {
+                "Acknowledge": "Acknowledge",
+                "Affirm": "Affirm",
+                "AlternateQuestions": "Alternate Question",
+                "Confirm": "Confirm",
+                "Deny": "Deny",
+                "FeedbackNegative": "Negative Feedback",
+                "FeedbackPositive": "Positive Feedback",
+                "Greetings/Salutations": "Greeting or Salutation",
+                "InformationOnObjectDetails": "Information on Object Details",
+                "InformationOther": "Other Information",
+                "Instruction": "Instruction",
+                "MiscOther": "Miscellaneous",
+                "NotifyFailure": "Notify user of Failure",
+                "OtherInterfaceComment": "Other Interface Comment",
+                "RequestForInstruction": "Request a new Instruction",
+                "RequestForObjectLocationAndOtherDetails": "Request for Object Information",
+                "RequestMore": "Request More Information",
+                "RequestOtherInfo": "Request Other Information",
+            }
+        else:
+            self.das = {
+                "Acknowledge": "Acknowledge",
+                "Affirm": "Affirm",
+                "AlternateQuestions": "AlternateQuestions",
+                "Confirm": "Confirm",
+                "Deny": "Deny",
+                "FeedbackNegative": "FeedbackNegative",
+                "FeedbackPositive": "FeedbackPositive",
+                "Greetings/Salutations": "Greetings/Salutations",
+                "InformationOnObjectDetails": "InformationOnObjectDetails",
+                "InformationOther": "InformationOther",
+                "Instruction": "Instruction",
+                "MiscOther": "MiscOther",
+                "NotifyFailure": "NotifyFailure",
+                "OtherInterfaceComment": "OtherInterfaceComment",
+                "RequestForInstruction": "RequestForInstruction",
+                "RequestForObjectLocationAndOtherDetails": "RequestForObjectLocationAndOtherDetails",
+                "RequestMore": "RequestMore",
+                "RequestOtherInfo": "RequestOtherInfo",
+            }
+
+        self.kwargs = kwargs
 
         self.tasks: list[dict] = [task for datafile in glob.glob(os.path.join(self.data_path, "*_turn.json"))
                                   for task in json.load(open(datafile, 'r'))]
@@ -24,8 +73,19 @@ class TurnMaker:
     def process_turn(self, turn: dict) -> str:
         driver_action = turn['DRIVER']['action'] if turn['DRIVER']['action'] != 'dialogue' else turn["DRIVER"]['utterance']
         commander_action = turn['COMMANDER']['action'] if turn['COMMANDER']['action'] != 'dialogue' else turn["COMMANDER"]['utterance']
-        return (f"<TURN> <{self.commander_name}> {commander_action} </{self.commander_name}>\n"
-                f"       <{self.driver_name}> {driver_action} </{self.driver_name}> </TURN>")
+        if turn['DRIVER']['action'] == 'dialogue':
+            driver_das = ','.join([self.das[da] for da in turn['DRIVER']['das']])
+        else:
+            driver_das = ""
+        if turn['COMMANDER']['action'] == 'dialogue':
+            commander_das = ','.join([self.das[da] for da in turn['COMMANDER']['das']])
+        else:
+            commander_das = ""
+        if self.html_format:
+            return (f"<TURN> <{self.commander_name}> {commander_action} {f'<<{commander_das}>> ' if self.include_das and commander_das else ''}</{self.commander_name}>\n"
+                    f"       <{self.driver_name}> {driver_action} {f'<<{driver_das}>> ' if self.include_das and driver_das else ''}</{self.driver_name}> </TURN>")
+        else:
+            return f"{self.commander_name}: {commander_action}{f' <<{commander_das}>>' if self.include_das and commander_das else ''}\n{self.driver_name}: {driver_action}{f' <<{driver_das}>>' if self.include_das and driver_das else ''}"
 
     def get_rand_turns(self, task: dict) -> list[str]:
         length = self.task_length if self.task_length is not None else random.randint(6, min(len(task['turns']) - 2, 25))
@@ -35,16 +95,29 @@ class TurnMaker:
 
     def make_prompt(self) -> str:
         prompt = ""
-        prompt += open("src/prompt_llm/prompt_segments/initial_instructions.txt", "r").read() + '\n\n'
+        prompt += open("src/prompt_llm/user_sim_segments/initial_instructions.txt", "r").read() + '\n\n'
+
+        if self.include_das or self.predict_das:
+            if self.nl_ify:
+                prompt += open("src/prompt_llm/user_sim_segments/da_nl_explain.txt", "r").read() + '\n\n'
+            else:
+                prompt += open("src/prompt_llm/user_sim_segments/da_explain.txt", "r").read() + '\n\n'
 
         tasks = random.sample(self.tasks, self.n + self.give_task)
 
         for i, task in enumerate(tasks[:self.n]):
             prompt += f"Example {i if self.numerate else ''}:\n"
             prompt += f"<goal> {task['goal']} </goal>\n"
-            prompt += "\n".join(self.get_rand_turns(task)) + '\n\n'
+            turns = self.get_rand_turns(task)
+            prompt += "\n".join(turns[:-1]) + '\n'
+            prompt += "Commander's Response:\n"
+            prompt += turns[-1].splitlines()[0] + '\n\n'
+            prompt += '\n\n'
 
-        prompt += open("src/prompt_llm/prompt_segments/final_instructions.txt", "r").read() + '\n\n'
+        if self.predict_das:
+            prompt += open("src/prompt_llm/user_sim_segments/final_da_instructions.txt", "r").read() + '\n\n'
+        else:
+            prompt += open("src/prompt_llm/user_sim_segments/final_instructions.txt", "r").read() + '\n\n'
 
         if self.give_task:
             prompt += "Give your answer for the following example:\n"
@@ -69,6 +142,10 @@ class TurnMaker:
 @click.option("--max_length", default=25, help="Maximum length of the examples")
 @click.option("--min_length", default=6, help="Minimum length of the examples")
 @click.option("--give_task", "-t", is_flag=True, help="Whether to give a task to the LLM to classify at the end")
+@click.option("--include_das", "-i", is_flag=True, help="Whether to include dialogue acts in the examples")
+@click.option("--nl_ify", "--nl", is_flag=True, help="Make dialogue acts be more natural language-y")
+@click.option("--html-format", is_flag=True, help="Whether to format the prompt in HTML")
+@click.option("--predict_das", "-p", is_flag=True, help="Whether to make the task be predicting dialogue acts")
 def main(**kwargs):
     tm = TurnMaker(**kwargs)
     tm.generate_prompt()
