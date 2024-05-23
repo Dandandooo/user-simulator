cbd5dd7b7e65cc7ae0934a43fb31f2a231674591
Daniel Philipov <dp33@illinois.edu>
2024-05-23 10:44:02 -0500

    bigger update

--- src/prompt_llm/runllama.py
+++ src/prompt_llm/runllama.py
@@ -8,5 +8,5 @@ llama3 = HugLM(model_name)
 llama3.data.add("zero", "llm_prompts_data/turns/valid_no_move/")
 llama3.data.add("five", "llm_prompts_data/turns/valid_5_no_move/")
 
-llama3.save_answers("zero", "llm_prompt_sessions/llama_no-train/zero_no_move/")
 llama3.save_answers("five", "llm_prompt_sessions/llama_no-train/five_no_move/")
+llama3.save_answers("zero", "llm_prompt_sessions/llama_no-train/zero_no_move/")
