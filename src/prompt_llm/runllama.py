27417eb5689139191612c4eaaa479b3c6c26d5da
Daniel Philipov <batkodanny@gmail.com>
2024-06-11 10:40:56 -0400

    gonna run llama again

--- src/prompt_llm/runllama.py
+++ src/prompt_llm/runllama.py
@@ -1,12 +1,12 @@
 from src.model.llms import HugLM
 
-model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
-# model_name = "unsloth/llama-3-8b-bnb-4bit"
+# model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
+model_name = "unsloth/llama-3-8b-Instruct-bnb-4bit"
 
 llama3 = HugLM(model_name)
 
-llama3.data.add("zero", "/projects/bckf/dphilipov/teach-recreate/llm_prompts_data/turns/valid_no_move/")
-llama3.data.add("five", "/projects/bckf/dphilipov/teach-recreate/llm_prompts_data/turns/valid_5_no_move/")
+llama3.data.load("5_no_move")
+llama3.save_answers("5_no_move", "test", "llm_prompt_sessions/llama_no-train/5_no_move.json")
 
-llama3.save_answers("five", "/projects/bckf/dphilipov/teach-recreate/llm_prompt_sessions/llama_no-train/five_no_move/")
-llama3.save_answers("zero", "/projects/bckf/dphilipov/teach-recreate/llm_prompt_sessions/llama_no-train/zero_no_move/")
+llama3.data.load("0_no_move")
+llama3.save_answers("0_no_move", "test", "llm_prompt_sessions/llama_no-train/0_no_move.json")
