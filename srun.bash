594fe411cb927baa77058b30692447a624c7f15e
Daniel Philipov <dp33@illinois.edu>
2024-06-17 11:38:23 -0500

    ran gpt

--- srun.bash
+++ srun.bash
@@ -1,1 +1,1 @@
-srun --account=bckf-delta-gpu --partition=gpuA40x4-interactive --time=1:00:00 --mem=16G --nodes=1 --gpus-per-node=4 --gpus=4 --pty bash
+srun --account=bckf-delta-gpu --partition=gpuA40x4-interactive --time=1:00:00 --mem=32G --nodes=1 --cpus-per-task=8 --gpus=1 --pty bash
