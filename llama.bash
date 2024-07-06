544fe2323a7460d6ee960a3e76ce237e99f81b92
Daniel Philipov <dp33@illinois.edu>
2024-07-06 10:13:34 -0500

    migration

--- llama.bash
+++ llama.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True PYTHONPATH=/projects/bckf/dphilipov/teach-recreate/ python src/prompt_llm/runllama.py
+PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True PYTHONPATH=/taiga/illinois/collab/eng/cs/conv-ai/UserSimulator python src/prompt_llm/runllama.py
--- llamatune.py
+++ llamatune.py
@@ -1,7 +1,8 @@
 from src.model.llms import LoraLM
 
 # model_name = "unsloth/llama-3-8b-Instruct-bnb-4bit"
-model_name = "unsloth/gemma-2b-it-bnb-4bit"
+# model_name = "unsloth/gemma-2b-it-bnb-4bit"
+model_name = "google/gemma-1.1-2b-it"
 
 llm = LoraLM(model_name)
 
